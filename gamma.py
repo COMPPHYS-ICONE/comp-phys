5e6aa610a8066aa8d5f5b38d2b8ee76954fe7b7f
COMPPHYS-ICONE <iccone@usfca.edu>
2015-09-17 19:25:33 -0700

    hi

--- gamma.py
+++ gamma.py
@@ -10,23 +10,27 @@ True
 >>> abs(abs(gamma(4)) - 6) < 1e-4
 True
 
->>> abs(abs(gamma(5./2)) - 1.3293403) < 1e-4
+>>> abs(abs(gamma(5./2)[0]) - 1.3293403) < 1e-4
 True
 
->>> abs(abs(gamma(7./2)) - 3.3233509) < 1e-4
+>>> abs(abs(gamma(7./2)[0]) - 3.3233509) < 1e-4
 True
 
-
+    Call signature  :
+    
+    python gamma.py -t 9.784 -tol .001 
+    
+    To run doctests in verbose mode:
+    
+    python -m doctest -v gamma.py
 
 '''
 
 from math import *
 import numpy
 import pdb
-total = 0
-last_total = 0
-dx = .1
-frac = ''
+
+steps = 100
 
 def h(x,t):
     return (x**(t-1))*(e**(-x))
@@ -37,26 +41,60 @@ def frac_diff():
     else:
         return abs(total-last_total)/total
 
-def gamma(t):
+def gamma(t,tol = 1e-4):
     if 100 >= t >= 1:
         if type(t) == int:
             return factorial(t-1)
         if type(t) == float:
-            global frac
-            frac = 'yes'
+            steps = 100
+            dx = 1.
             total = 0
-            for x in numpy.linspace (0, 1000, 10001):
-                total += (dx/6)*(h(x,t) +h(x+dx,t) +4*h((2*x+dx)/2,t))
-                if x == 900:
-                    last_total = total
-            return float(total)
+            last_total = 0
+            fractional_diff = 1000
+            #pdb.set_trace()
+            while fractional_diff > tol:
+                last_total = total
+                total = 0
+                for x in numpy.linspace (0.0000000001, 100, steps+1):
+                    total += (dx/6)*(h(x,t) +h(x+dx,t) +4*h((2*x+dx)/2,t))
+                fractional_diff = abs(total-last_total)/total
+                steps *=2
+                dx /=2
+            return float(total), last_total, fractional_diff
     else:
         return 'Please enter a value between 1 and 100.'
     
+
+
+    
 if __name__ =="__main__":
-    t = (7./2)
+    frac = ''
+    fractional_diff = 0
+    total = 0
+    last_total = 0
+    import argparse
     import doctest
-    doctest.testmod()    
-    print 'Gamma of',t,'is:',gamma(t)
-    if frac == 'yes':
-        print 'frac_diff =', "{:0.7f}".format(frac_diff()*100),"%"
+    #doctest.testmod()
+    
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-t', type = float)
+    parser.add_argument('-tol', type = float)
+    args = parser.parse_args()
+    t = args.t
+    tol = args.tol
+
+    stringofT = str(t)
+    
+    if stringofT[-1] == '0':
+        t = int(t)
+        
+    
+    
+    if type(t) == int:
+        fact = gamma(t,tol)
+        print 'Gamma of',t,'is:',fact
+    if type(t) == float:
+        total, last_total, fractional_diff = gamma(t,tol)
+        print 'Gamma of',t,'is:',total
+        print 'frac_diff =', "{:0.7f}".format(fractional_diff*100),"%"
+
