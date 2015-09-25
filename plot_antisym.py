d826a1855f381609638963e800c2bf459a842c06
COMPPHYS_ICONE <iccone@usfca.edu>
2015-09-24 17:05:19 -0700

    hi

--- plot_antisym.py
+++ plot_antisym.py
@@ -22,7 +22,7 @@ Call signature  :
 '''
 
 def antisym(x1, x2, n1 = 1, n2 = 2, a = 1.0):
-    psi = (2/a)*((np.sin(n1*np.pi*x1/a)*np.sin(n2*np.pi*x1/a)) - (np.sin(n1*np.pi*x2/a)*np.sin(n2*np.pi*x1/a)))
+    psi = (2/a)*((np.sin(n1*np.pi*x1/a)*np.sin(n2*np.pi*x2/a)) - (np.sin(n1*np.pi*x2/a)*np.sin(n2*np.pi*x1/a)))
     return psi, (psi**2)
     
 if __name__ == "__main__":
@@ -47,30 +47,24 @@ if __name__ == "__main__":
     fig = plt.figure()
     plt.suptitle('Antisymmetric Spatial Wave Function')
     ax = fig.add_subplot(121, projection = '3d')
-    x = y = np.linspace(0,1.0,100)
+    x = y = np.linspace(-x1,x2,100)
     xv, yv = np.meshgrid(x,y)
     wave_f, prob_dens = antisym(xv,yv,n1 = 1, n2 = 2, a = 1.0)#2 rows, 1 column, first of the two
-    #ax.plot_wireframe(xv, yv, z, rstride=1, cstride=1, linewidth = 1)
-    ax.plot_surface(xv, yv, wave_f, rstride=3, cstride=3, cmap=cm.OrRd, linewidth = 0)
+    ax.plot_surface(xv, yv, wave_f, rstride=2, cstride=2, cmap=cm.coolwarm, linewidth = .1)
     fig.subplots_adjust(top=0.85) # without this, won't be able to see plot title(moves plot down a little bit)
     plt.title('Wave Function')
     ax.set_xlabel('x')
     ax.set_ylabel('y')
 
-    #plt.plot(x, y1, 'r.', lw = 2)
-    #plt.grid(True)
 
-    bx = plt.subplot(122, projection ='3d')# 2 rows, 1 column, second of two       #both ways are perfectly legal
-    #ax.plot_wireframe(xv, yv, z, rstride=10, cstride=10, linewidth = 1)
-    bx.plot_surface(xv, yv, prob_dens, rstride=3, cstride=3, cmap=cm.OrRd, linewidth = 0)
+    bx = plt.subplot(122, projection ='3d')# 2 rows, 1 column, second of two       
+    bx.plot_surface(xv, yv, prob_dens, rstride=2, cstride=2, cmap=cm.coolwarm, linewidth = .1)
     bx.set_xlabel('x')
     bx.set_ylabel('y')
-    fig.subplots_adjust(top=0.95)
+    fig.subplots_adjust(top=0.9)
     plt.title('Probability Density')
-    plt.figtext(.1, .04, 'The "effective" interaction between two neutral Fermions (both spin up) that arises from the\n symmetry requirement on the total wave function.')
+    plt.figtext(.07, .03, 'The "effective" interaction between two neutral Fermions (both spin up) that arises from the\n symmetry requirement on the total wave function.')
     
-    #plt.xlabel('x')
-    #plt.ylabel('y2')
 
     plt.show()
     
